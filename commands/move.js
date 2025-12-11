48deb2d70adfaaed25ad39c66a55636e5c5d4cec
dadzerdous <dadzaverous@gmail.com>
2025-12-11 07:29:00 -0500

    Update move.js

--- commands/move.js
+++ commands/move.js
@@ -1,16 +1,34 @@
-
 module.exports = {
     name: "move",
     aliases: ["go"],
-    execute(player, args, players, rooms) {
-        const dir = args[0];
-        const room = rooms[player.room];
-
-        if (!room.exits[dir]) {
-            return player.ws.send("You cannot go that way.");
+    execute(socket, sess, accounts, world, arg) {
+        if (!arg) {
+            return socket.send(JSON.stringify({ type: "system", msg: "Move where?" }));
         }
 
-        player.room = room.exits[dir];
-        commands.look.execute(player, [], players, rooms);
+        const dir = arg.toLowerCase();
+        const room = world[sess.room];
+
+        if (!room || !room.exits || !room.exits[dir]) {
+            return socket.send(JSON.stringify({ type: "system", msg: "You cannot go that way." }));
+        }
+
+        const newRoom = room.exits[dir];
+        sess.room = newRoom;
+
+        if (accounts[sess.loginId]) {
+            accounts[sess.loginId].lastRoom = newRoom;
+        }
+
+        const newRoomObj = world[newRoom];
+
+        socket.send(JSON.stringify({
+            type: "room",
+            id: newRoom,
+            title: newRoomObj.title,
+            desc: newRoomObj.text,
+            exits: Object.keys(newRoomObj.exits || {}),
+            background: newRoomObj.background || null
+        }));
     }
 };
